27819f0bff05009762f08d41f5cd6fd47233e815
Jonathan Schenker <jonathan.f.schenker@vanderbilt.edu>
2016-01-29 21:18:52 -0600

    Added twitch chat system

---
+++ Application/Assets/Twitch/TwitchDemo.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TwitchDemo : MonoBehaviour {
+
+	private TwitchIRC IRC;
+	public Text txt;
+	int X;
+
+	void Start()
+    {
+        IRC = FindObjectOfType<TwitchIRC>();
+        //IRC.SendCommand("CAP REQ :twitch.tv/tags"); //register for additional data such as emote-ids, name color etc.
+        IRC.messageRecievedEvent.AddListener(OnChatMsgRecieved);
+    }
+
+	void OnChatMsgRecieved(string msg)
+    {
+        //parse from buffer.
+        int msgIndex = msg.IndexOf("PRIVMSG #");
+        string msgString = msg.Substring(msgIndex + IRC.channelName.Length + 11);
+        string user = msg.Substring(1, msg.IndexOf('!') - 1);
+
+        Debug.Log(msgString);
+
+        if(msgString == "vote")
+        {
+        	X++;
+        }
+
+    }
+
+    void Update()
+    {
+    	txt.text = ""+X;
+    }
+
+
+}
